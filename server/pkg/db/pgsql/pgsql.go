5cc99c53a6e6ad7cd1c82032bb740334c7cce4a8
userIT <>
2025-07-20 15:09:19 +0500

    Поменял константы на переменные окружения

--- server/pkg/db/pgsql/pgsql.go
+++ server/pkg/db/pgsql/pgsql.go
@@ -9,11 +9,12 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-const (
-	host   = "localhost"
-	port   = 5432
-	user   = "postgres"
-	dbname = "postgres"
+var (
+	host   = os.Getenv("PGSQL_HOST")
+	port   = os.Getenv("PGSQL_PORT")
+	user   = os.Getenv("PGSQL_USER")
+	dbname = os.Getenv("PGSQL_DBNAME")
+	pswPg  = os.Getenv("PGSQL_PSW")
 )
 
 type User struct {
@@ -34,9 +35,8 @@ func selDataUser(userData User) (DataUser, bool) {
 
 	var us DataUser
 	ctx := context.Background()
-	pswPg := os.Getenv("PGSQL_PSW") // "11111111"
 
-	connString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, pswPg, dbname)
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pswPg, dbname)
 	conn, err := pgx.Connect(ctx, connString)
 
 	query := `select id_user, email from users where username = $1;`
@@ -63,9 +63,8 @@ func SelDataUser(userData User) (DataUser, bool) {
 func chUserExist(userData User) bool {
 	var check bool
 	ctx := context.Background()
-	pswPg := os.Getenv("PGSQL_PSW")
 
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, pswPg, dbname)
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pswPg, dbname)
 	conn, err := pgx.Connect(ctx, connStr)
 
 	query := `SELECT user_exists($1, $2);`
@@ -97,9 +96,8 @@ func AddNewUser(u User) bool {
 func addNewUser(userData User) bool {
 	var in int
 	ctx := context.Background()
-	pswPg := os.Getenv("PGSQL_PSW")
 
-	connString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, pswPg, dbname)
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pswPg, dbname)
 	conn, err := pgx.Connect(ctx, connString)
 
 	query := `INSERT INTO users (username, pass, email) VALUES ($1, $2, $3) RETURNING id_user;`
